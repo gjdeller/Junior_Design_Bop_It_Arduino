ff325c1a8d2880f34017966a5687297d2bdea493
Garrett Deller <144945880+gjdeller@users.noreply.github.com>
2025-10-19 22:26:06 -0400

    Add comments to ReactorPhysics class
    Added comments to ReactorPhysics class for clarity.

--- ReactorProject/ReactorPhysics.h
+++ ReactorProject/ReactorPhysics.h
@@ -1,6 +1,9 @@
 #ifndef REACTOR_PHYSICS_H
 #define REACTOR_PHYSICS_H
 
+// Reactor physics class. Needed to make instances of each reactor. 
+// Still ahve to implement in real time changing of the reactor and having 
+// independent values for each reactor. Will investigate later
 class ReactorPhysics {
 public:
     ReactorPhysics();   // constructor (no arguments needed)
